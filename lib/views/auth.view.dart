8d50792c14c1be913e267378c9f733b382717536
JotaDouglas <josantos.inv@gmail.com>
2024-05-08 20:13:00 -0300

    feat: raio no container

--- lib/views/auth.view.dart
+++ lib/views/auth.view.dart
@@ -5,17 +5,24 @@ class AuthPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final _screen = MediaQuery.of(context).size;
+    final _radio = _screen.width * 0.3;
+    return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          // Container(
-          //   height: 100,
-          //   width: double.infinity,
-          //   color: Colors.white,
-          // ),
+          Container(
+            height: _screen.height * 0.8,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(_radio),
+              ),
+              color: Colors.white,
+            ),
+          ),
         ],
-      ), 
-      );
+      ),
+    );
   }
 }
