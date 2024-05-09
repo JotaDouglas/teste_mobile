4dfeb081d22e7469f8ad24d18d863b6be543aec5
JotaDouglas <josantos.inv@gmail.com>
2024-05-08 22:07:56 -0300

    feat: icon color

--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -8,46 +8,59 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  goBack(){
+  goBack() {
     Navigator.of(context).pop();
   }
+
   @override
-  
   Widget build(BuildContext context) {
     final screen = MediaQuery.of(context).size;
-    final _radio = screen.width * 0.1;
+    final radio = screen.width * 0.1;
     return Scaffold(
       appBar: AppBar(
-        leading: IconButton(onPressed: (){}, icon: const Icon(Icons.arrow_back_ios_rounded)),
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back_ios_rounded),
+          color: Colors.white,
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
-           Padding(
-             padding: const EdgeInsets.only(left: 5),
-             child: Container(
+          Padding(
+            padding: const EdgeInsets.only(left: 10),
+            child: SizedBox(
               width: screen.width,
               height: screen.height * 0.1,
               child: const Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [                
-                  Text("HELLO", style: TextStyle(color: Colors.white, fontSize: 17),),
-                  Text("User", style: TextStyle(color: Colors.white, fontSize: 25),),
+                children: [
+                  Text(
+                    "HELLO",
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  ),
+                  Text(
+                    "Username",
+                    style: TextStyle(color: Colors.white, fontSize: 25),
+                  ),
                 ],
               ),
-                       ),
-           ),
-          SizedBox(height: screen.height * 0.05,),
+            ),
+          ),
+          SizedBox(
+            height: screen.height * 0.05,
+          ),
           Container(
-            height: screen.height * 0.75, 
+            height: screen.height * 0.75,
             width: screen.width,
-            child: Center(child: Text("Hello")),               
+            child: Center(child: Text("Hello")),
             decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(_radio),
-                    topRight: Radius.circular(_radio)),
-                    color: Colors.white,),
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(radio),
+                  topRight: Radius.circular(radio)),
+              color: Colors.white,
+            ),
           )
         ],
       ),
